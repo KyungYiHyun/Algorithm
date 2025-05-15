01210b1debbc386c509fa990feb716747d0bfd2c
KyungYiHyun <140145081+KyungYiHyun@users.noreply.github.com>
2025-05-15 13:40:43 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.13 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int[] dp = new int[n + 2];
+        dp[1] = 1;
+        dp[2] = 2;
+        if (n == 1){
+            return 1;
+        }
+        if (n == 2){
+            return 2;
+        }
+        for (int i = 3; i < n + 1; i++){
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
+        }
+        answer = dp[n];
+        return answer;
+    }
+}
