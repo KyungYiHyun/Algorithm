ce362e09c1b11ed667c91b97af35bfa897dd0f51
KyungYiHyun <140145081+KyungYiHyun@users.noreply.github.com>
2025-06-09 16:11:21 +0900

    [level 2] Title: 더 맵게, Time: 1652.42 ms, Memory: 124 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -1,27 +1,34 @@
-import java.util.PriorityQueue;
+import java.util.*;
 
 class Solution {
     public int solution(int[] scoville, int K) {
         int answer = 0;
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for (int s : scoville) {
-            pq.add(s);
+        
+        Queue<Integer> pq = new PriorityQueue<>();
+ 
+        for (int i = 0; i < scoville.length; i++){
+            pq.add(scoville[i]);
         }
-        while (true) {
-            int fp = pq.poll();
-            if (fp >= K) {
+        
+        while(pq.size() > 1){   
+            int first = pq.poll();
+            
+            if (first >= K){
                 break;
             }
-            if (pq.isEmpty()){
-                answer = -1;
-                break;
-            }
+            
+            int second = pq.poll();
+            int newScovile = first + second * 2;
             answer++;
-            int sp = pq.poll();
-            pq.add(fp + 2 * sp);
+            pq.add(newScovile);
         }
-
-
+        
+        if (pq.size() == 1){
+            if (pq.poll() < K){
+                return -1;
+            }
+        }
+        
         return answer;
     }
 }
