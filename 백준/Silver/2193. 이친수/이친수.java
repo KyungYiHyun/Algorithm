8a3e8e8396f07b47d607a4ecc19129be00a1af17
KyungYiHyun <140145081+KyungYiHyun@users.noreply.github.com>
2025-07-03 20:30:45 +0900

    [Silver III] Title: 이친수, Time: 104 ms, Memory: 14272 KB -BaekjoonHub

---
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        long[][] dp = new long[91][2];
+
+        dp[1][0] = 0;
+        dp[1][1] = 1;
+        dp[2][0] = 1;
+        dp[2][1] = 0;
+        for (int i = 3; i < 91; i++) {
+            dp[i][0] = dp[i - 1][0] + dp[i - 1][1];
+            dp[i][1] = dp[i - 1][0];
+        }
+
+        System.out.println(dp[n][0] + dp[n][1]);
+
+    }
+
+
+}
+
+
+
