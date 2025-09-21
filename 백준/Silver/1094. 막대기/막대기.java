3fa17e5e34c008a5b248ebb45114d7959ca5d883
KyungYiHyun <140145081+KyungYiHyun@users.noreply.github.com>
2025-09-21 14:28:02 +0900

    [Silver V] Title: 막대기, Time: 108 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Silver/1094. 막대기/막대기.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int cnt = 0;
+
+        for (int i = 8; i >= 0; i--) {
+            if (N == 0) break;
+            if (N >= Math.pow(2, i)) {
+                N -= (int) Math.pow(2, i);
+                cnt++;
+            }
+
+        }
+        System.out.println(cnt);
+    }
+}
